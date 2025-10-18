d4e281334dfae7c820b2e9bad6542d9acb06e825
0xleander <utcjpygj4026@hotmail.com>
2025-10-18 18:51:40 +0800

    redesign header

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,20 +1,4 @@
-import { ConnectButton } from '@rainbow-me/rainbowkit';
-import '../styles/Header.css';
-
 export function Header() {
-  return (
-    <header className="header">
-      <div className="header-container">
-        <div className="header-content">
-          <div className="header-left">
-            <h1 className="header-title">
-              
-            </h1>
-            
-          </div>
-          <ConnectButton />
-        </div>
-      </div>
-    </header>
-  );
+  // Header is now integrated into TradeApp component
+  return null;
 }
